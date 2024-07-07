f621dff7171afdbffc6036da43cdadc58dabbd3f
AZ70code <alexzubov1970@gmail.com>
2024-07-07 11:15:51 +0100

    created App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import React from "react";
+import Container from "./Components/Container/container";
+import Todos from "./Components/Todos/todos";
+import json from "./db.json";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => (
+  <div className="app">
+    <Container>
+      <Todos data={json.TABLE_CONTENT} />
+    </Container>
+  </div>
+);
 
 export default App;
