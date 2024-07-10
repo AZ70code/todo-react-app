c33f6f2a304749475053b98fe6eca72eecdb729f
AZ70code <alexzubov1970@gmail.com>
2024-07-10 16:33:37 +0100

    created control panel

--- src/Components/ControlPanel/controlPanel.jsx
+++ src/Components/ControlPanel/controlPanel.jsx
@@ -1,23 +1,39 @@
 import React from "react";
-import style from './controlPanel.module.css';
+import style from "./controlPanel.module.css";
 import { Button } from "@mui/material";
+import Filter from "../Filter/filter";
 
-const ControlPanel = ({total, archived, completed}) => {
-	return (
-		<div className={style.control}>
-			<h2 className={style.title}>Todo info</h2>
-			<div className={style.inner}>
-			<div className={style.info}>
-				<p>Total todo: {total}</p>
-				<p>Completed: {completed}</p>
-				<p>Archived: {archived}</p>
-			</div>
-			<div className={style.buttongroup}>
-				<Button color="success" children="ADD NOTE" variant="contained" size="large"/>
-				<Button color="warning" variant="contained" size="large">ARCHIV</Button>
-			</div>
-			</div>
-		</div>
-	)
-}
+const ControlPanel = ({ total, archived, completed, onArchive, onForm, filterValue, onFilterCompleted, onFilterCategory }) => {
+  return (
+    <div className={style.control}>
+      <div className={style.inner}>
+        <div className={style.info}>
+          <div className={style.output}>
+            <h2 className={style.title}>Todo info</h2>
+            <p>Total todo: {total}</p>
+            <p>Completed: {completed}</p>
+            <p>Archived: {archived}</p>
+          </div>
+          <div className={style.border}></div>
+          <div className={style.filter}>
+            <h2 className={style.title}>Filter</h2>
+            <Filter filterValue={filterValue} onFilterCompleted={onFilterCompleted} onFilterCategory={onFilterCategory} />
+          </div>
+        </div>
+        <div className={style.buttongroup}>
+          <Button
+            color="success"
+            children="ADD NOTE"
+            variant="contained"
+            size="large"
+            onClick={onForm}
+          />
+          <Button color="warning" variant="contained" size="large" onClick={onArchive}>
+            ARCHIV
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default ControlPanel;
