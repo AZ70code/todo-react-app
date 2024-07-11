fa9712e6a6aad39ffd328bfdcfd38ffccbaacce2
AZ70code <alexzubov1970@gmail.com>
2024-07-11 15:18:11 +0100

    feature close popUp by backdrop click

--- src/Components/Modal/modal.jsx
+++ src/Components/Modal/modal.jsx
@@ -6,10 +6,14 @@ import Form from "../Form/form";
 const modalRoot = document.getElementById("modal-root");
 
 const Modal = ({ onSubmit, onCloseModal }) => {
+	const handleBackdropClick = (e) => {
+		e.target === e.currentTarget && onCloseModal()
+	}
+
   return createPortal(
-    <div className={style.modal}>
+    <div className={style.modal} onClick={handleBackdropClick} >
 			<h1>Create New Note</h1>
-      <Form onSubmit={onSubmit} onCloseModal={onCloseModal} />
+      <Form onSubmit={onSubmit} onCloseModal={onCloseModal}/>
     </div>,
     modalRoot
   );
